3b2f799853027c23d98b3bcc5aa582599269e3f4
Futafi <D.futagi@vivaldi.net>
2019-04-30 23:20:35 +0900

    [add] いろいろな店で商品を検索してurlを手に入れるためのメソッドのひな形を追加

--- moyashi/book.py
+++ moyashi/book.py
@@ -1,9 +1,12 @@
 from .photo import Photo
+import requests
+import json
 
 
 class Book:
-    def __init__(self, vision_api_key="", title="", isbn13=None, isbn10=None, url_rakuten="", url_amazon=""):
-        self.vision_api_key = vision_api_key
+    def __init__(self, vision_api_key="", title="", isbn13=None, isbn10=None,
+                 url_rakuten="", url_amazon="", url_google=""):
+        self.photo = Photo(vision_api_key)
         self.title = title
         self.title_gps = ""
         self.title_vision = ""
@@ -13,4 +16,21 @@ class Book:
         self.isbn10 = isbn10
         self.url_rakuten = url_rakuten
         self.url_amazon = url_amazon
+        self.url_google_books = url_google
+        self.google_books_base_url = "https://www.googleapis.com/books/v1/volumes?q=intitle:"
         self.match_rate = 0
+
+    def search_in_rakuten(self, title: str = ""):
+        if title:
+            self.title = title
+        return self.url_rakuten
+
+    def search_in_amazon(self, title: str = ""):
+        if title:
+            self.title = title
+        return self.url_amazon
+
+    def search_in_google_books(self, title: str = ""):
+        if title:
+            self.title = title
+        return self.url_google_books
